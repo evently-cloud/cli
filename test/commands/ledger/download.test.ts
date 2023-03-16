22db348a98928f672b8d0ad52156083543984be0
Evert Pot <me@evertpot.com>
2023-03-16 17:22:48 -0400

    Things work with Ketting 8!

--- test/commands/ledger/download.test.ts
+++ test/commands/ledger/download.test.ts
@@ -6,7 +6,6 @@ const tempDir = "./tmp/test"
 const ledgerFile = `${tempDir}/test-download.ndjson`
 const testToken = "test-token"
 const firstEventId = "0005d5813bf9c4498e2a8b74e95bff80"
-import { Request, Response } from 'node-fetch';
 
 import { setMockCallback, initClient } from '../../../src/lib/client';
 
@@ -30,26 +29,52 @@ describe("ledger:download", async () => {
         case '/ledgers/download' :
           expect(req.method).to.equal('POST');
           expect(req.headers.get('Prefer')).to.equal('return=representation');
-          return new Response(
-            JSON.stringify({
-              entity:    "test",
-              key:       "first-event-key",
-              event:     "tested",
-              eventId:   firstEventId,
-              timestamp: "2022-01-14T02:05:17.275209Z",
-              meta:      {
-                actor: "Evently CLI Tester"
-              },
-              data:      {
-                tested:      true,
-                description: "This first event is for validating ledger downloads."
+
+          const body = await req.json();
+
+          if (body.after === undefined) {
+            return new Response(
+              JSON.stringify({
+                entity:    "test",
+                key:       "first-event-key",
+                event:     "tested",
+                eventId:   firstEventId,
+                timestamp: "2022-01-14T02:05:17.275209Z",
+                meta:      {
+                  actor: "Evently CLI Tester"
+                },
+                data:      {
+                  tested:      true,
+                  description: "This first event is for validating ledger downloads."
+                }
+              }),
+              {
+                status: 200
               }
-            }),
-            {
-              status: 200
-            }
-          );
-          break;
+            );
+          }
+          if (body.after === firstEventId) {
+            return new Response(
+              JSON.stringify({
+                entity:    "test",
+                key:       "second-event-key",
+                event:     "tested",
+                eventId:   "0005d5814ce488843dc5de3ee95bff80",
+                timestamp: "2022-01-14T02:10:01.096324Z",
+                meta:      {
+                  actor: "Evently CLI Tester"
+                },
+                data:      {
+                  tested:      true,
+                  description: "This is a second event after the initial download."
+                }
+              }),
+              {
+                status: 200
+              }
+            );
+            break;
+          } else throw new Error('Unexpected body');
         case '/' : 
           return new Response(
             JSON.stringify({
@@ -83,15 +108,7 @@ describe("ledger:download", async () => {
           return new Response('{}', {status: 501});
       }
 
-    }) as any);
-
-
-    /*
-    const client = initClient(testToken);
-    const state = await client.go().get();
-    console.log(state.links.getAll());
-    throw new Error('foo');
-    */
+    }));
 
     test
       .stdout()
@@ -100,31 +117,6 @@ describe("ledger:download", async () => {
         expect(ctx.stdout).to.contain("Downloading ledger fully")
         expect(ctx.stdout).to.contain("Validated 1 ledger event")
       })
-
-    // @ts-ignore
-    global.mockEvently.intercept({
-      path: "/ledgers/download",
-      method: "POST",
-      headers: {
-        authorization: `Bearer ${testToken}`,
-        prefer:        "return=representation"
-      },
-      // this is the full download
-      body:   `{"after":"${firstEventId}"}`
-    }).reply(200, {
-      entity:    "test",
-      key:       "second-event-key",
-      event:     "tested",
-      eventId:   "0005d5814ce488843dc5de3ee95bff80",
-      timestamp: "2022-01-14T02:10:01.096324Z",
-      meta:      {
-        actor: "Evently CLI Tester"
-      },
-      data:      {
-        tested:      true,
-        description: "This is a second event after the initial download."
-      }
-    })
 
     test
       .stdout()
